7c175f4b83ad02380c4a708bfd22a219d3865dd0
thenixontorres <nixontorres2@gmail.com>
2021-02-04 13:46:36 -0400

    lesson 12

---
+++ resources/views/course.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Detalles del curso</h1>
+    </div>
+    <livewire:course-list>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [PageController::class,'index'])->name('home');
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::get('/curso', [PageController::class,'course'])->name('course');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
